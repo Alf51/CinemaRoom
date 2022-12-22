af99619e544eff68de93bd01ec49070a191a239e
Vasiliy <zakupm51@gmail.com>
2022-12-22 20:27:57 +0300

    refactoring

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,22 +1,12 @@
 import java.util.Scanner;
 
-// прочитай в конце код минимум 4 раза !! и подумай ! Мб что-то поменять ?
-// в конце скопипую, создай свой проект, а потм на ГИТХАБ
-// обработай исключения, если вводишь буквы вместо цифр, сейчас выбрасывает
-/* может отдельный метод для сканера ? Чтобы возвращал цифру, там же и обработает и мб закроется ? Но если он будет постоянно
-открываться и закрываться это будет топу ! Мб видос про рефакторинг поможет !
-проверить название методов, они тебе понятны ?
-В конце после того как создашь и перенесёшь код в свой проект, попробуй его скомпилировать и запустить )
-Перечитать код в пятый раз и убрать коментарии, оставить один или два и то, если они нужны
-*/
 public class Cinema {
-    //проверить, все ли глобальные переменные нужны
-    static String[][] CINEMA_HALL; // можно ли избавится от глобальных переменных ?
-    static Scanner SCANNER = new Scanner(System.in); // это вообще бред, подумай как убрать сканер и нормально ли его так оставлять ?
-    static int TICKETS_PURCHASED = 0;
-    static int ALL_SEATS;
-    static int CURRENT_INCOME = 0;
-    static int TOTAL_INCOME = 0;
+    static String[][] CINEMA_HALL;
+    static Scanner SCANNER = new Scanner(System.in);
+    static int ticketsPurchased = 0;
+    static int allSeats;
+    static int currentIncome = 0;
+    static int totalIncome = 0;
     final static int FRONT_ROWS_SEAT_PRICE = 10;
     final static int BACK_ROWS_SEAT_PRICE = 8;
 
@@ -36,6 +26,7 @@ public class Cinema {
                     3. Statistics
                     0. Exit
                     """);
+
             switch (SCANNER.nextInt()) {
                 case 1 -> showCinemaHall();
                 case 2 -> buyTicket();
@@ -45,17 +36,14 @@ public class Cinema {
         }
     }
 
-    //вот тут проверка, чтобы люди вводили ТОЛЬКО цифры и не отрицательные
     static String[][] createCinemaHall() {
         String[][] cinemaHall;
-        boolean finishEnter = false;
-        while (!finishEnter) {
             System.out.println("Enter the number of rows:");
             final int rows = SCANNER.nextInt() + 1;
             System.out.println("Enter the number of seats in each row:");
             final int seats = SCANNER.nextInt() + 1;
             cinemaHall = new String[rows][seats];
-        }
+
         for (int i = 0; i < cinemaHall.length; i++) {
             for (int y = 0; y < cinemaHall[i].length; y++) {
                 if (i == 0 && y == 0) cinemaHall[i][y] = " ";
@@ -65,7 +53,7 @@ public class Cinema {
             }
         }
 
-        ALL_SEATS = (rows - 1) * (seats - 1); // если All seats будет ниже, то мы не увидим общую прибыль за все сиденья, нужно отрефакторить, хотя это в методе и вроде норм
+        allSeats = (rows - 1) * (seats - 1);
         showTotalIncome(cinemaHall);
         return cinemaHall;
     }
@@ -80,16 +68,15 @@ public class Cinema {
         }
     }
 
-    //продумай, когда пользовотель передумает брать, может место ему не понравилось ? Добавить вариант выхода
+
     static void buyTicket() {
         boolean finishBuying = false;
         int row = 0;
-        int seat = 0;
+        int seat;
 
         while (!finishBuying) {
             System.out.println("Enter a row number:");
             System.out.println("or enter 0 for exit:");
-            finishBuying = SCANNER.nextInt() == 0;
 
             row = SCANNER.nextInt();
             System.out.println("Enter a seat number in that row:");
@@ -101,13 +88,13 @@ public class Cinema {
 
     static void countPrice(int row) {
         //if there are less than 60 available seats = we take the maximum price
-        if (ALL_SEATS < 60) {
-            CURRENT_INCOME += FRONT_ROWS_SEAT_PRICE;
+        if (allSeats < 60) {
+            currentIncome += FRONT_ROWS_SEAT_PRICE;
             System.out.printf("Ticket price: $%d\n", FRONT_ROWS_SEAT_PRICE);
         } else {
             int frontRowLine = Math.max((CINEMA_HALL.length - 1) / 2, 4);
             int currentPrice = row <= frontRowLine ? FRONT_ROWS_SEAT_PRICE : BACK_ROWS_SEAT_PRICE;
-            CURRENT_INCOME += currentPrice;
+            currentIncome += currentPrice;
             System.out.printf("Ticket price: $%d\n", currentPrice);
         }
     }
@@ -121,14 +108,13 @@ public class Cinema {
             return false;
         } else {
             CINEMA_HALL[row][seat] = "B";
-            TICKETS_PURCHASED++;
+            ticketsPurchased++;
             return true;
         }
     }
 
     static void showStatistic() {
-
-        double PERCENTAGE_TICKETS_PURCHASED = TICKETS_PURCHASED != 0 ? 100 / ((double)ALL_SEATS / TICKETS_PURCHASED) : 0;
+        double PERCENTAGE_TICKETS_PURCHASED = ticketsPurchased != 0 ? 100 / ((double) allSeats / ticketsPurchased) : 0;
 
         System.out.printf("""
                 
@@ -137,20 +123,19 @@ public class Cinema {
                 Current income: $%d
                 Total income: $%d
                 
-                """, TICKETS_PURCHASED, PERCENTAGE_TICKETS_PURCHASED, CURRENT_INCOME, TOTAL_INCOME);
+                """, ticketsPurchased, PERCENTAGE_TICKETS_PURCHASED, currentIncome, totalIncome);
     }
 
-    //отрефактори, ты используешь похожий код 2 раза ( в countPrice). Можно метод общий сделать
     static void showTotalIncome(String[][] cinemaHall) {
         //if there are less than 60 available seats = we take the maximum price
-        if (ALL_SEATS < 60) {
-            TOTAL_INCOME = cinemaHall.length * cinemaHall[0].length * FRONT_ROWS_SEAT_PRICE;
+        if (allSeats < 60) {
+            totalIncome = cinemaHall.length * cinemaHall[0].length * FRONT_ROWS_SEAT_PRICE;
         } else {
             int row = cinemaHall.length - 1;
             int seats = cinemaHall[0].length - 1;
             int frontRowLine = Math.max((row - 1) / 2, 4);
             int backRowLine = row - frontRowLine;
-            TOTAL_INCOME = frontRowLine * FRONT_ROWS_SEAT_PRICE * seats + backRowLine * BACK_ROWS_SEAT_PRICE * seats;
+            totalIncome = frontRowLine * FRONT_ROWS_SEAT_PRICE * seats + backRowLine * BACK_ROWS_SEAT_PRICE * seats;
         }
     }
 }
